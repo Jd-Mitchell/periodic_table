79e53e76f8e7fb7dab59c1a00c4c2590c3a99c33
= <jdpmitchell@gmail.com>
2024-09-19 00:01:59 -0400

    feat: setup the sql query for the periodic table with the needed columns

--- element.sh
+++ element.sh
@@ -4,3 +4,6 @@
 
 PSQL="psql -X --username=freecodecamp --dbname=periodic_table  --tuples-only -c"
 
+SQL_QUERY="SELECT elements.atomic_number, elements.symbol, elements.name, properties.atomic_mass, properties.melting_point_celsius, properties.boiling_point_celsius, types.type FROM elements INNER JOIN properties using(atomic_number) INNER JOIN types USING(type_id)"
+
+
